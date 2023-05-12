fff484f19d6887ec2fc4a6e394ebb13e10ff1070
Kim Ji Hun <gmldnr2222@naver.com>
2023-05-12 17:58:11 +0900

    feat: mv pkg

--- src/main/java/com/jikim/cafekiosk/spring/CafekioskApplication.java
+++ src/main/java/com/jikim/cafekiosk/spring/CafekioskApplication.java
@@ -1,4 +1,4 @@
-package com.jikim.cafekiosk;
+package com.jikim.cafekiosk.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
